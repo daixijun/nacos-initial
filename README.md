04ded5e64ba2400019e75f2897e88d5091984bf6
Xijun Dai <daixijun1990@gmail.com>
2021-10-28 12:05:24 +0800

    refactor: 移除对 ACM 支持
    由于阿里 ACM 服务即将下线
    故放弃 ACM 转开源 Nacos
    
    BREAKING CHANGE: 不再支持 ACM

--- README.md
+++ README.md
@@ -1,32 +1,27 @@
 # nacos-initial
 
-用于CICD中拉取ACM/Nacos中的配置到本地
+用于 CICD 中拉取 Nacos 中的配置到本地配置文件
 
 docker image: daixijun1990/nacos-initial
-
-> 目前暂不支持ACM中使用`KMS AES-128`加密过的配置
 
 参数:
 
 ```shell
 $ ./nacos-initial --help
 Usage of ./nacos-initial:
-  -ak string
-      阿里云RAM子账号 accessKey
   -dataId string
-      Nacos/ACM dataId
-  -endpoint string
-      Nacos/ACM 服务器地址 (default "acm.aliyun.com")
+        Nacos dataId
   -group string
-      配置组 (default "DEFAULT_GROUP")
-  -kms
-      开启KMS自动解密配置内容
-  -ns string
-      Nacos/ACM 命名空间ID
+        Nacos Group (default "DEFAULT_GROUP")
+  -namespace string
+        Nacos namespace id
   -o string
-      输出配置到终端或者文件 (default "stdout")
-  -regionId string
-      KMS所在区域 (default "cn-hangzhou")
-  -sk string
-      阿里云RAM子账号 secretKey
+        output to config file or stdout (default "stdout")
+  -password string
+        Nacos Password
+  -server string
+        Nacos server addr
+  -username string
+        Nacos Username
+
 ```
--- main.go
+++ main.go
@@ -4,75 +4,53 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/verystar/nacos-go-sdk"
 )
 
 var (
-	endpoint    string
+	server      string
 	namespaceId string
-	accessKey   string
-	secretKey   string
+	username    string
+	password    string
 	dataId      string
 	group       string
 	output      string
-	openKMS     bool
-	regionId    string
 )
 
 func init() {
-	flag.StringVar(&endpoint, "endpoint", "acm.aliyun.com", "Nacos/ACM 服务器地址")
-	flag.StringVar(&namespaceId, "ns", "", "Nacos/ACM 命名空间ID")
-	flag.StringVar(&accessKey, "ak", "", "阿里云RAM子账号 accessKey")
-	flag.StringVar(&secretKey, "sk", "", "阿里云RAM子账号 secretKey")
-	flag.StringVar(&dataId, "dataId", "", "Nacos/ACM dataId")
-	flag.StringVar(&group, "group", "DEFAULT_GROUP", "配置组")
-	flag.StringVar(&output, "o", "stdout", "输出配置到终端或者文件")
-	flag.BoolVar(&openKMS, "kms", false, "开启KMS自动解密配置内容")
-	flag.StringVar(&regionId, "regionId", "cn-hangzhou", "KMS所在区域")
+	flag.StringVar(&server, "server", "", "Nacos server addr")
+	flag.StringVar(&namespaceId, "namespace", "", "Nacos namespace id")
+	flag.StringVar(&username, "username", "", "Nacos Username")
+	flag.StringVar(&password, "password", "", "Nacos Password")
+	flag.StringVar(&dataId, "dataId", "", "Nacos dataId")
+	flag.StringVar(&group, "group", "DEFAULT_GROUP", "Nacos Group")
+	flag.StringVar(&output, "o", "stdout", "output to config file or stdout")
 	flag.Parse()
 }
 
 func main() {
-
-	clientConfig := constant.ClientConfig{
-		Endpoint:       endpoint + ":8080",
-		NamespaceId:    namespaceId,
-		AccessKey:      accessKey,
-		SecretKey:      secretKey,
-		TimeoutMs:      5 * 1000,
-		ListenInterval: 30 * 1000,
-		OpenKMS:        openKMS,
-		RegionId:       regionId,
-	}
-
-	configClient, err := clients.CreateConfigClient(map[string]interface{}{
-		"clientConfig": clientConfig,
+	conf := nacos.NewNacosConfig(func(c *nacos.NacosConfig) {
+		c.Username = username
+		c.Password = password
+		c.ServerAddr = server
 	})
 
-	if err != nil {
-		panic(fmt.Sprintf("初始化客户端失败: %s\n", err.Error()))
-	}
-
-	// 获取配置
-	content, err := configClient.GetConfig(vo.ConfigParam{
-		DataId: dataId,
-		Group:  group},
-	)
+	data, err := conf.Get(namespaceId, group, dataId)
 
 	if err != nil {
-		panic(fmt.Sprintf("获取配置失败: %s\n", err.Error()))
+		log.Fatalf("get config failed: %s\n", err)
 	}
 
 	if output == "stdout" {
-		fmt.Printf("Get config:\n%s\n", content)
+		fmt.Printf("get config: %s\n", data)
 	} else {
-		err = ioutil.WriteFile(output, []byte(content), 0644)
+		err = ioutil.WriteFile(output, []byte(data), 0644)
 		if err != nil {
-			panic(fmt.Sprintf("写配置文件失败: %s\n", err.Error()))
+			log.Fatalf("write config to file failed: %s\n", err.Error())
 		}
-		fmt.Println("配置成功写入文件: ", output)
+		fmt.Println("write config to file succeed: ", output)
 	}
+
 }
