d6a145020703328efbe4bdc5bdff0333aec8962e
Xijun Dai <daixijun1990@gmail.com>
2024-01-16 15:31:10 +0800

    feat: Update dependencies
    Signed-off-by: Xijun Dai <daixijun1990@gmail.com>

--- go.mod
+++ go.mod
@@ -3,14 +3,11 @@ module github.com/daixijun/nacos-initial
 go 1.17
 
 require (
-	github.com/verystar/nacos-go-sdk v1.1.3
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	github.com/alecthomas/kingpin/v2 v2.4.0
+	github.com/verystar/nacos-go-sdk v1.2.0
 )
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 	"runtime"
 
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/verystar/nacos-go-sdk"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	server      = kingpin.Flag("server", "Nacos server addr").Short('s').Envar("NACOS_SERVER").String()
-	namespaceId = kingpin.Flag("namespace-id", "Nacos namespace id").Short('n').Envar("NACOS_NAMESPACE_ID").String()
-	username    = kingpin.Flag("username", "nacos username").Short('u').Envar("NACOS_USERNAME").String()
-	password    = kingpin.Flag("password", "Nacos password").Short('p').Envar("NACOS_PASSWORD").String()
-	dataId      = kingpin.Flag("data-id", "Nacos data id").Short('d').Envar("NACOS_DATA_ID").String()
+	server      = kingpin.Flag("server", "Nacos server addr").Short('s').Envar("NACOS_SERVER").Required().String()
+	namespaceId = kingpin.Flag("namespace-id", "Nacos namespace id").Short('n').Envar("NACOS_NAMESPACE_ID").Required().String()
+	username    = kingpin.Flag("username", "nacos username").Short('u').Envar("NACOS_USERNAME").Required().String()
+	password    = kingpin.Flag("password", "Nacos password").Short('p').Envar("NACOS_PASSWORD").Required().String()
+	dataId      = kingpin.Flag("data-id", "Nacos data id").Short('d').Envar("NACOS_DATA_ID").Required().String()
 	group       = kingpin.Flag("group", "Nacos group name").Short('g').Default("DEFAULT_GROUP").Envar("NACOS_GROUP").String()
 	output      = kingpin.Flag("output", "Output to config file or stdout").Short('o').Default("stdout").Envar("NACOS_OUTPUT").String()
 
@@ -50,7 +50,7 @@ platform: %s/%s`, version, revision, builtDate, goVersion, runtime.GOOS, runtime
 	if *output == "stdout" {
 		fmt.Printf("get config: %s\n", data)
 	} else {
-		err = ioutil.WriteFile(*output, []byte(data), 0644)
+		err = os.WriteFile(*output, []byte(data), 0644)
 		if err != nil {
 			log.Fatalf("write config to file failed: %s\n", err.Error())
 		}
